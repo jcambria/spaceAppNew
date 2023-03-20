5d5512149581471810b73439dc3ef2b2b84359d1
Ayub <egalayub2@gmail.com>
2023-03-20 14:42:35 -0400

    Main Program With SpringBoot

---
+++ JAC/JAC/src/main/java/com/example/JAC/JacApplication.java
@@ -0,0 +1,13 @@
+package com.example.JAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacApplication.class, args);
+	}
+
+}
